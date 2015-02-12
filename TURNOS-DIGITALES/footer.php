c07670f0dfe13dedb8f4506eeccd77b586674a37
Kevin Santacruz <defonix0@gmail.com>
2015-02-12 12:37:29 -0500

    Eliminacion archivos

--- TURNOS-DIGITALES/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-	<h1 class="foot">CITE-MATICO</h1>
-	<span class="foot">Sistema de citas para atención de padres de familia</span>
-	<h2 class="foot">EDCOM-ESPOL</h2>
-</footer>
--- pages/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-		<h1 class="foot">CITE-MATICO</h1>
-		<span class="foot">Sistema de citas para atención de padres de familia</span>
-		<h2 class="foot">EDCOM-ESPOL</h2>
-	</footer>
