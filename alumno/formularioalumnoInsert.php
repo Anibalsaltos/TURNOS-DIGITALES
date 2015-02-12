9e9a8e42bbaf943e394f17037fd1f6f2446f4f4c
Christian Brunis <Christianazul16@hotmail.com>
2015-02-11 23:04:17 -0500

    clase alumno

---
+++ alumno/formularioalumnoInsert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>formulario alumno</title>
+</head>
+<body>
+<form action="insert.php" method="post" >
+<p>
+
+Primer Nombre: <input type="text" name="Primer Nombre" autofocus required />
+
+Segundo Nombre: <input type="text" name="Segundo Nombre" />	
+
+Primer Apellido: <input type="text" name="Primer Apellido"  />
+
+Segundo Apellido: <input type="text" name="Segundo Apellido"  />
+
+</p>
+<input type="submit" value="Submit!" />
+
+</form>
