267e5ad6d25c672284d21c1a9827c24e8a06fb57
jowpris <rickcolla@hotmail.com>
2015-02-25 13:34:14 -0500

    Pagina consultar

---
+++ consultar.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<title>Cite Matico - Consultar Datos</title>
+	</head>
+	<body>
+		<header class="navegacion">
+			<nav class="navbar navbar-default" role="navigation">
+			      	<h1 class="titulo"><img id="logo" src="img/logo.png" alt="CITE-MATICO"/> CITE-MATICO</h1>
+			</nav>
+		</div>
+		</header>
+		
+		<section class="datos">
+			<div>
+				<h1>Institucion</h1>
+				<div><a href="#" class="nuevo">Nuevo</a></div>
+				<div class="institucion">
+					<h2>ID</h2>
+					<h2>Descripcion</h2>
+					<h2>Estado</h2>
+					<h2>Opciones</h2>
+				</div>
+				<div class="datos-institucion">
+					<div>1</div>
+					<div>Institucion 1</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-institucion">
+					<div>2</div>
+					<div>Institucion 2</div>
+					<div>1</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-institucion">
+					<div>3</div>
+					<div>Institucion 3</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+			</div>
+
+			<div>
+				<h1>Salon</h1>
+				<div><a href="#" class="nuevo">Nuevo</a></div>
+				<div class="salon">
+					<h2>ID</h2>
+					<h2>Descripcion</h2>
+					<h2>Estado</h2>
+					<h2>Opciones</h2>
+				</div>
+				<div class="datos-salon">
+					<div>1</div>
+					<div>Salon 1</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-salon">
+					<div>2</div>
+					<div>Salon 2</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-salon">
+					<div>3</div>
+					<div>Salon 3</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+			</div>
+
+			<div>
+				<h1>Representante</h1><div><a href="#" class="nuevo">Nuevo</a></div>	
+				<div class="representante">
+					<h2>ID</h2>
+					<h2>Nombres</h2>
+					<h2>Apelidos</h2>
+					<h2>Convencional</h2>
+					<h2>Celular</h2>
+					<h2>Direccion</h2>
+					<h2>Email</h2>
+					<h2>Estado</h2>
+					<h2>Opciones</h2>
+				</div>
+				<div class="datos-representante">
+					<div>1</div>
+					<div>Kevin</div>
+					<div>Santacruz</div>
+					<div>666666</div>
+					<div>0981893287</div>
+					<div>Francisco Jacome</div>
+					<div>defonix0@gmail.com</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-representante">
+					<div>2</div>
+					<div>Joel</div>
+					<div>Collahuazo</div>
+					<div>1369</div>
+					<div>0981893287</div>
+					<div>Francisco Jacome</div>
+					<div>jowpris@gmail.com</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+				<div class="datos-representante">
+					<div>3</div>
+					<div>Anibal</div>
+					<div>Saltos</div>
+					<div>222</div>
+					<div>0981893287</div>
+					<div>Francisco Jacome</div>
+					<div>anibal@gmail.com</div>
+					<div>0</div>
+					<div><a href="#">Editar</a></div>
+					<div><a href="#">Eliminar</a></div>
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
