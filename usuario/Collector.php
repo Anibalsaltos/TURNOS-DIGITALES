b655b26ac07c4783f238640ec88a1d0714f45fb9
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2015-02-26 10:36:31 -0500

    Objetos para la entidad Usuario

--- usuario/Collector.php
+++ usuario/Collector.php
@@ -5,7 +5,7 @@ include_once('dataBase.php');
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "root");
-define("DB_NAME", "clasedb");
+define("DB_NAME", "citas");
 
 
 class Collector extends dataBase
